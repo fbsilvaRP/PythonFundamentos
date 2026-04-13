d25149310d8e6fb9cc89160ba42254412b9beb01
Felipe Braga <febragasilva@outlook.com>
2026-04-13 20:56:20 -0300

    Update EX01 - Exercício resolvido.py

--- Capítulo 6/Exercícios resolvidos/EX01 - Exercício resolvido.py
+++ Capítulo 6/Exercícios resolvidos/EX01 - Exercício resolvido.py
@@ -1,11 +1,19 @@
 #Feito por Felipe Braga
 #Última edição em 13 de abril de 2026
 
+import random
+
 vetor1 = []
+
+''' Preenchendo o vetor manualmente
 
 #Preenchendo o vetor
 for i in range(9):
     vetor1.append(int(input("Digite um número inteiro: ")))
+    
+'''
+#Preenchendo o vetor com valores aleatórios
+
 
 #Exibindo o vetor
 print("-=" * 25)
